367ddc0485d4b271a0b52421191ae579860c23b8
SamuraJey <47861714+SamuraJey@users.noreply.github.com>
2021-04-26 13:28:35 +0500

    Больше комментариев

--- procedural_bot.py
+++ procedural_bot.py
@@ -5,19 +5,21 @@ import vk_api
 from num2words import num2words
 from vk_api.longpoll import VkLongPoll, VkEventType
 
+# Различные словари для ответов бота
 goodbye = ["пока", "пока-пока", "возвращайся скорее", "прощай", "до скорых встреч", "асталависта", "до свидания",
            "до новых встреч"]
 
 hello = ["приветствую", "здравствуй", "привет", "привет-привет", "проходи не задерживайся", "хай", "давно не виделись",
          "ты смотри кто к нам колеса катит. Привет"]
 
-who_are_you = ['кто ты?', "что ты?", "кто твой создатель?", 'кто ты', "что ты", "кто твой создатель"
+who_are_you = ['кто ты?', "что ты?", "кто твой создатель?", 'кто ты', "что ты", "кто твой создатель",
                "ты робот?", "ты бот?"]
 
 n_help = ["помощь", "что ты умеешь?", "команды", "список команд", "что ты умеешь",
           "в чем твоя польза?", "в чём твоя польза?", "в чем твоя польза", "в чём твоя польза"]
 
 
+# функция написания сообщения пользователю для прикрепления фотографий используется attachment по умолчанию равный False
 def write_msg(user_id, message, attachment=False):
     if not attachment:
         vk.method('messages.send', {'user_id': user_id, 'message': message, 'random_id': random.randint(0, 2 ** 32)})
@@ -26,22 +28,23 @@ def write_msg(user_id, message, attachment=False):
                                     'random_id': random.randint(0, 2 ** 32)})
 
 
-# API-ключ созданный ранее
+# API-ключ для группы
 token = "APIKEY"
 
 # Авторизуемся как сообщество
 vk = vk_api.VkApi(token=token)
 
-
 # Работа с сообщениями
 longpoll = VkLongPoll(vk)
 
+# переменные для работы генератора паролей
 pas_use = []
 low_let = 'abcdefghijkmnpqrstuvwxyz'
 high_let = 'ABCDEFGHJKLMNPQRSTUVWXYZ'
 nums = '0123456789'
 
 
+# Функция открывающая файл с цитатами и возвращающая случайную
 def quotes():
     text = []
     with open('data/text/quotes.txt',
@@ -50,6 +53,7 @@ def quotes():
         return random.choice(text)
 
 
+# Функция открывающая файл с анекдотами и возвращающий случайный
 def anekdot():
     jokes = []
     with open('data/text/jokes.txt',
@@ -58,6 +62,7 @@ def anekdot():
         return random.choice(jokes)
 
 
+# Функция открывающая директорию с фотографиями и возвращающая случайную
 def photo():
     upload = vk_api.VkUpload(vk)
     photo = upload.photo_messages('data/img/lolcats' + str(random.randint(1, 80)) + '.jpg')
@@ -68,6 +73,7 @@ def photo():
     return attachment
 
 
+# Функция Генерирующая пароли
 def generate_password(m):
     end = False
     while end is False:
@@ -100,6 +106,7 @@ def get_passwords(n, m):
     return ', '.join(passwords)
 
 
+# Функция работающая с API OWM и возвращающая строку с погодой в указанном городе
 def get_weather(city):
     url = 'http://api.openweathermap.org/data/2.5/weather?q=' + \
           city + '&units=metric&lang=ru&appid=6183cc27ea7a57941943927547658652'
@@ -115,6 +122,7 @@ def get_weather(city):
     return line
 
 
+# Функция возвращает текущее время по которому работает бот
 def get_time():
     today = datetime.today()
     wd = date.weekday(today)
@@ -125,6 +133,7 @@ def get_time():
            f"Время {today.hour}:{today.minute}"
 
 
+# Списки для работы функций шифровки/дешифровки шифра цезаря
 llst = ['а', 'б', 'в', 'г', 'д', 'е', 'ж', 'з', 'и', 'й', 'к', 'л', 'м', 'н',
         'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ъ', 'ы',
         'ь', 'э', 'ю', 'я']
@@ -133,6 +142,7 @@ blst = ['А', 'Б', 'В', 'Г', 'Д', 'Е', 'Ж', 'З', 'И', 'Й', 'К', 'Л', 
         'Ь', 'Э', 'Ю', 'Я']
 
 
+# Функция шифровки по алгоритму цезаря
 def encrypt_caesar(msg, shift):
     ret = ""
     shift = int(shift)
@@ -148,6 +158,7 @@ def encrypt_caesar(msg, shift):
     return ret
 
 
+# Функция шифровки по алгоритму цезаря
 def decrypt_caesar(msg, shift):
     ret = ""
     shift = int(shift)
@@ -163,9 +174,9 @@ def decrypt_caesar(msg, shift):
     return ret
 
 
-print("Бот запущен")
+# print("Бот запущен")
 
-# флаги
+# флаги для работы условий
 weather = False
 password_quan = False
 password = False
@@ -179,7 +190,7 @@ for event in longpoll.listen():
     # Если пришло новое сообщение
     if event.type == VkEventType.MESSAGE_NEW:
 
-        # Если оно имеет метку для меня( то есть бота)
+        # Если оно имеет метку для меня(то есть бота)
         if event.to_me:
 
             # Сообщение от пользователя
@@ -197,22 +208,23 @@ for event in longpoll.listen():
             # активируем навык "генератор паролей"
             elif request == 'генератор паролей':
                 write_msg(event.user_id, 'Введите длину пароля (Минимум 3)')
-                # флаг "password" нужен для того, чтобы следующее соо юзера воспринималось именно как длина пароля
+                # флаг "password" нужен для того, чтобы следующее сообщение воспринималось именно как длина пароля
+                # следующие навки реализованы по такому же принципу
                 password = True
             # если "password" true, значит пользователь активировал навык генерации паролей и сейчас ввел длину пароля
             elif password:
-                # проверяем правильно ли тупой юзер все ввел
+                # проверяем правильно ли пользователь все ввел
                 if request.isdigit():
                     password_m = int(request)
                 else:
-                    # если юзер тупой то деактивируем навык
+                    # если данные не подходят  деактивируем навык
                     write_msg(event.user_id, 'Что-то не то...')
                     password = False
                     continue
                 write_msg(event.user_id, 'Введите количество паролей (Минимум 1)')
                 password_quan = True
                 password = False
-            # если "password_quan" true, значит юзер активировал навык генерации паролей и сейчас ввел кол-во паролей
+            # если "password_quan" true, значит активирован навык генерации паролей и сейчас вводится кол-во паролей
             elif password_quan:
                 if request.isdigit():
                     password_n = int(request)
@@ -224,18 +236,19 @@ for event in longpoll.listen():
                 write_msg(event.user_id, 'Готово: ' + get_passwords(password_n, password_m))
                 # меняем флаг на false, теперь навык не активирован
                 password_quan = False
-
+            # Реакция на сообщение с запросом помощи / списком комманд
             elif request in n_help:
                 write_msg(event.user_id, 'Список комманд: \n генератор паролей - позволяет сгенерировать ' +
-                                         'один или несколько паролей \n время - скажу тебе дату и время по которым я' +
-                                         ' работаю и живет мой создатель \n цезарь - позволит тебе расшифровать ' +
-                                         'или зашифровать сообщение шифром цезаря \n погода - позволит тебе узнать ' +
-                                         'текущую погоду в указаном тобой городе ' +
-                                         '(только не ошибайся в названии городов) \n число словами - напишет тебе как' +
-                                         ' пишется введенное тобой число русскими словами \n коты - отправит тебе' +
-                                         ' картинку с котом или кошкой, наслаждайся \n цитаты - ' +
-                                         'случайная цитата из фильма, всего 100 штук \n анедоты - ' +
-                                         'пришлет тебе какой то анекдот')
+                          'один или несколько паролей \n время - скажу тебе дату и время по которым я' +
+                          ' работаю и живет мой создатель \n цезарь - позволит тебе расшифровать ' +
+                          'или зашифровать сообщение шифром цезаря \n погода - позволит тебе узнать ' +
+                          'текущую погоду в указаном тобой городе ' +
+                          '(только не ошибайся в названии городов) \n число словами - напишет тебе как' +
+                          ' пишется введенное тобой число русскими словами \n коты - отправит тебе' +
+                          ' картинку с котом или кошкой, наслаждайся \n цитаты - ' +
+                          'случайная цитата из фильма, всего 100 штук \n анедоты - ' +
+                          'пришлет тебе какой то анекдот')
+
             elif request == "цитаты":
                 write_msg(event.user_id, f'Твоя цитата: \n {quotes()}')
 
@@ -267,7 +280,7 @@ for event in longpoll.listen():
                 else:
                     write_msg(event.user_id, 'Не понял вашего ответа...')
                     caesar = not caesar
-
+            # используется библеотека num2words для того что бы перевести число в слова
             elif request == "число словами":
                 write_msg(event.user_id, 'Введите какое число вы хотите написать словами')
                 num_to_words = True
@@ -275,16 +288,16 @@ for event in longpoll.listen():
                 print(request)
                 write_msg(event.user_id, num2words(int(request), lang='ru'))
                 num_to_words = False
-
+                # Навык коты
             elif request == 'коты':
                 write_msg(event.user_id, "Твой котан:", photo())
-
+                # навык анедоты
             elif request == 'анекдоты':
                 write_msg(event.user_id, f'Твоя шутка: \n {anekdot()}')
-
+                # навык отвечающий на вопрос "кто ты?" и т.п.
             elif request in who_are_you:
                 write_msg(event.user_id, "Я процедурный Бот-013, мой создатель Сергей написал меня на " +
-                                         "Python используя кучу if'ов и elif'ов")
+                          "Python используя кучу if'ов и elif'ов")
 
             # активируем навык "погода"
             elif request == 'погода':
@@ -297,5 +310,6 @@ for event in longpoll.listen():
                 write_msg(event.user_id, get_weather(request))
                 # деактивируем навык изменением флага
                 weather = not weather
+                # самый последний случай если ничего из вышеперечисленного не подходит, сообщаем юзеру что не поняли его
             else:
-                write_msg(event.user_id, "Не поняла вашего ответа...")
+                write_msg(event.user_id, "Не понял вашего ответа...")
